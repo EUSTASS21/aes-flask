137c458d15e0ad6a205b943af7be66851b1771a9
eeustass <aymane.amchiya2013@gmail.com>
2024-11-17 17:34:49 +0100

    postgresql

--- securedrive/models.py
+++ securedrive/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserFolder(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)  # One-to-one relationship with the User model
+    folder_id = models.CharField(max_length=255, null=True, blank=True)  # Folder ID to assign to the user
+
+    def __str__(self):
+        return f"{self.user.username}'s folder ID: {self.folder_id}"
